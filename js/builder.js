7ecfeddf971c390c43bad77d249979ad83c43f64
Yana Lunina <yanalunina96@gmail.com>
2024-11-26 16:43:38 +0200

    add js | fix content | styles

---
+++ js/builder.js
@@ -0,0 +1,35 @@
+const loadHeadHTML = (url, selector) => {
+  fetch(url)
+    .then((response) => response.text())
+    .then((html) => {
+      const elements = document.querySelectorAll(selector);
+      elements.forEach((element) => {
+        element.innerHTML = html;
+      });
+    });
+};
+loadHeadHTML("head.html", ".head");
+
+const loadHeaderHTML = (url, selector) => {
+  fetch(url)
+    .then((response) => response.text())
+    .then((html) => {
+      const elements = document.querySelectorAll(selector);
+      elements.forEach((element) => {
+        element.innerHTML = html;
+      });
+    });
+};
+loadHeaderHTML("header.html", ".header");
+
+const loadFooterHTML = (url, selector) => {
+  fetch(url)
+    .then((response) => response.text())
+    .then((html) => {
+      const elements = document.querySelectorAll(selector);
+      elements.forEach((element) => {
+        element.innerHTML = html;
+      });
+    });
+};
+loadFooterHTML("footer.html", ".footer");
--- js/mob-menu.js
+++ js/mob-menu.js
@@ -1,13 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const burgerMenu = document.querySelector(".burger-menu");
-  const mobileMenu = document.querySelector(".mobile-menu");
-  const closeMenu = document.querySelector(".close-menu");
+  const initializeMenu = () => {
+    const burgerMenu = document.querySelector(".burger-menu");
+    const mobileMenu = document.querySelector(".mobile-menu");
+    const closeMenu = document.querySelector(".close-menu");
+    const overlay = document.querySelector(".mobile-menu-overlay");
 
-  burgerMenu.addEventListener("click", () => {
-    mobileMenu.style.left = "0";
+    if (burgerMenu && mobileMenu && closeMenu && overlay) {
+      
+      burgerMenu.addEventListener("click", () => {
+        mobileMenu.style.left = "0";
+        overlay.classList.add("active"); 
+      });
+
+      closeMenu.addEventListener("click", () => {
+        mobileMenu.style.left = "100%";
+        overlay.classList.remove("active"); 
+      });
+
+
+      overlay.addEventListener("click", () => {
+        mobileMenu.style.left = "100%";
+        overlay.classList.remove("active"); 
+      });
+    }
+  };
+
+  // Ініціалізація логіки
+  initializeMenu();
+
+  // Спостереження за змінами в DOM
+  const observer = new MutationObserver(() => {
+    if (
+      document.querySelector(".burger-menu") &&
+      document.querySelector(".mobile-menu-overlay")
+    ) {
+      initializeMenu();
+      observer.disconnect(); // Зупиняємо спостереження після успішної ініціалізації
+    }
   });
 
-  closeMenu.addEventListener("click", () => {
-    mobileMenu.style.left = "100%";
-  });
+  observer.observe(document.body, { childList: true, subtree: true });
 });
